1946adbc978e7f2988d460d8427d7394ecf7e097
Adkins <adkinsmwangi@gmail.com>
2025-03-18 13:03:09 +0300

    learnt about different collections

---
+++ collections.py
@@ -0,0 +1,8 @@
+# list
+scores = [65, 39, 86, 94, 80, 73, 68, 87]
+
+# dictionary
+details = {"name": "Adkins", "age": "17", "gender": "Male", "height": "1.87"}
+
+# set
+vowels = ("a", "e", "i", "o", "u")
