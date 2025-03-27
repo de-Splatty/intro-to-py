45d58fe70a76808a5003c87b112563848f2536db
Adkins <adkinsmwangi@gmail.com>
2025-03-27 14:41:34 +0300

    learnt about lambda functions

---
+++ lambda_functions.py
@@ -0,0 +1,97 @@
+# lambda functions
+
+def add_numbers(a, b):
+    return a + b
+
+
+print(add_numbers(2, 3))
+
+add_two = lambda x, y: x + y
+
+print(add_two(12, 45))
+
+multiply_three = lambda a, b, c: a * b * c
+
+print(multiply_three(12, 45, 10))
+
+students = [{"id": 1, "names": "Valene de Leon", "gender": "Female", "maths": 78, "english": 86, "kiswahili": 57,
+             "chemistry": 71, "biology": 63, "accounting": 68, "history": 45},
+            {"id": 2, "names": "Rhodia Batterson", "gender": "Female", "maths": 66, "english": 80, "kiswahili": 53,
+             "chemistry": 19, "biology": 67, "accounting": 28, "history": 23},
+            {"id": 3, "names": "Lillis Searston", "gender": "Female", "maths": 52, "english": 47, "kiswahili": 57,
+             "chemistry": 35, "biology": 72, "accounting": 58, "history": 58},
+            {"id": 4, "names": "Selle Stobbie", "gender": "Female", "maths": 30, "english": 55, "kiswahili": 24,
+             "chemistry": 69, "biology": 79, "accounting": 44, "history": 46},
+            {"id": 5, "names": "Hansiain Tackes", "gender": "Male", "maths": 36, "english": 13, "kiswahili": 77,
+             "chemistry": 19, "biology": 48, "accounting": 43, "history": 49},
+            {"id": 6, "names": "Gaby Gierhard", "gender": "Male", "maths": 21, "english": 37, "kiswahili": 63,
+             "chemistry": 30, "biology": 65, "accounting": 58, "history": 61},
+            {"id": 7, "names": "Chlo Garment", "gender": "Female", "maths": 21, "english": 21, "kiswahili": 64,
+             "chemistry": 51, "biology": 28, "accounting": 39, "history": 64},
+            {"id": 8, "names": "Stavro McNally", "gender": "Male", "maths": 13, "english": 40, "kiswahili": 73,
+             "chemistry": 36, "biology": 72, "accounting": 35, "history": 70},
+            {"id": 9, "names": "Ber Porteous", "gender": "Male", "maths": 17, "english": 22, "kiswahili": 49,
+             "chemistry": 43, "biology": 29, "accounting": 65, "history": 36},
+            {"id": 10, "names": "Ilario Nation", "gender": "Male", "maths": 57, "english": 82, "kiswahili": 86,
+             "chemistry": 43, "biology": 25, "accounting": 87, "history": 56},
+            {"id": 11, "names": "Kippie Woodroffe", "gender": "Male", "maths": 12, "english": 56, "kiswahili": 36,
+             "chemistry": 19, "biology": 66, "accounting": 54, "history": 56},
+            {"id": 12, "names": "Monty Melato", "gender": "Male", "maths": 62, "english": 24, "kiswahili": 14,
+             "chemistry": 22, "biology": 28, "accounting": 43, "history": 69},
+            {"id": 13, "names": "Alexandrina Rebbeck", "gender": "Female", "maths": 66, "english": 18, "kiswahili": 75,
+             "chemistry": 53, "biology": 41, "accounting": 88, "history": 50},
+            {"id": 14, "names": "Tymothy Sarle", "gender": "Male", "maths": 15, "english": 31, "kiswahili": 40,
+             "chemistry": 40, "biology": 53, "accounting": 41, "history": 75},
+            {"id": 15, "names": "Marti Eriksson", "gender": "Female", "maths": 43, "english": 32, "kiswahili": 59,
+             "chemistry": 26, "biology": 22, "accounting": 31, "history": 16},
+            {"id": 16, "names": "Hoebart Ducroe", "gender": "Male", "maths": 47, "english": 19, "kiswahili": 13,
+             "chemistry": 62, "biology": 81, "accounting": 62, "history": 73},
+            {"id": 17, "names": "Dela Bruckshaw", "gender": "Female", "maths": 44, "english": 30, "kiswahili": 41,
+             "chemistry": 34, "biology": 34, "accounting": 82, "history": 60},
+            {"id": 18, "names": "Sylvan Mapstone", "gender": "Male", "maths": 31, "english": 43, "kiswahili": 44,
+             "chemistry": 33, "biology": 55, "accounting": 19, "history": 38},
+            {"id": 19, "names": "Noelani Fleury", "gender": "Female", "maths": 56, "english": 25, "kiswahili": 68,
+             "chemistry": 27, "biology": 40, "accounting": 32, "history": 32},
+            {"id": 20, "names": "Gillie Roma", "gender": "Female", "maths": 70, "english": 67, "kiswahili": 60,
+             "chemistry": 13, "biology": 44, "accounting": 78, "history": 32},
+            {"id": 21, "names": "Maxy Abelson", "gender": "Male", "maths": 75, "english": 57, "kiswahili": 21,
+             "chemistry": 20, "biology": 65, "accounting": 63, "history": 18},
+            {"id": 22, "names": "Eolande Rogez", "gender": "Female", "maths": 82, "english": 41, "kiswahili": 49,
+             "chemistry": 19, "biology": 90, "accounting": 85, "history": 59},
+            {"id": 23, "names": "Gonzales Foch", "gender": "Male", "maths": 54, "english": 41, "kiswahili": 28,
+             "chemistry": 10, "biology": 75, "accounting": 29, "history": 32},
+            {"id": 24, "names": "Gretna McGrouther", "gender": "Female", "maths": 30, "english": 17, "kiswahili": 72,
+             "chemistry": 64, "biology": 48, "accounting": 50, "history": 85},
+            {"id": 25, "names": "Brad Sprowson", "gender": "Male", "maths": 85, "english": 42, "kiswahili": 71,
+             "chemistry": 10, "biology": 54, "accounting": 42, "history": 57},
+            {"id": 26, "names": "Keir Tyght", "gender": "Male", "maths": 25, "english": 36, "kiswahili": 37,
+             "chemistry": 34, "biology": 85, "accounting": 11, "history": 39},
+            {"id": 27, "names": "Shirline Garrattley", "gender": "Female", "maths": 54, "english": 51, "kiswahili": 49,
+             "chemistry": 27, "biology": 53, "accounting": 28, "history": 42},
+            {"id": 28, "names": "Latrena Goskar", "gender": "Female", "maths": 53, "english": 13, "kiswahili": 24,
+             "chemistry": 66, "biology": 15, "accounting": 54, "history": 49},
+            {"id": 29, "names": "Barby McGerraghty", "gender": "Female", "maths": 19, "english": 16, "kiswahili": 37,
+             "chemistry": 32, "biology": 56, "accounting": 55, "history": 54},
+            {"id": 30, "names": "Frances Roughan", "gender": "Female", "maths": 65, "english": 46, "kiswahili": 12,
+             "chemistry": 22, "biology": 76, "accounting": 43, "history": 41},
+            {"id": 31, "names": "Vergil Sturton", "gender": "Male", "maths": 53, "english": 40, "kiswahili": 68,
+             "chemistry": 63, "biology": 74, "accounting": 37, "history": 19},
+            {"id": 32, "names": "Lucretia Yitzhakov", "gender": "Female", "maths": 83, "english": 44, "kiswahili": 51,
+             "chemistry": 32, "biology": 90, "accounting": 15, "history": 13}]
+
+male_students = filter(lambda student: student['gender'] == 'Male', students)
+
+sorted_by_chem = sorted(male_students, key=lambda s: s['chemistry'], reverse=True)
+
+print(sorted_by_chem[0:5])
+
+ages = [21, 24, 16, 17, 19, 25]
+
+def increment_age(age):
+    return age + 1
+
+new_ages = map(increment_age, ages)
+
+new_ages_2 = map(lambda a: a+1, ages)
+print(list(new_ages))
+
